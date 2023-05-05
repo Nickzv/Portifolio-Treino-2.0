24048b61b69c868d8b750ba8da39989bb14b430a
Izabelly <95751660+Nickzv@users.noreply.github.com>
2023-05-05 12:57:10 -0300

    Add files via upload

---
+++ Portifólio 2/script.js
@@ -0,0 +1,26 @@
+/* background squares */
+const ulSquares = document.querySelector("ul.squares");
+
+for (let i = 0; i < 950; i++) {
+  const li = document.createElement("li");
+
+  const random = (min, max) => Math.random() * (max - min) + min;
+
+  const size = Math.floor(random(1, 3));
+  const position = random(1, 99);
+  const delay = random(1, 0.1);
+  const duration = random(200, 2);
+
+  li.style.width = `${size}px`;
+  li.style.height = `${size}px`;
+  li.style.bottom = `-${size}px`;
+
+  li.style.left = `${position}%`;
+
+  li.style.animationDelay = `${delay}s`;
+  li.style.animationDuration = `${duration}s`;
+  li.style.animationTimingFunction = `cubic-bezier(${Math.random()}, ${Math.random()}, ${Math.random()}, ${Math.random()})`;
+
+  ulSquares.appendChild(li);
+}
+
